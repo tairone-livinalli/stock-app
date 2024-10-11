39b4db79238501f271e16a80ec7522ac253d6b39
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-10-11 19:59:08 -0400

    chore: add styled components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {ThemeProvider} from 'styled-components/native';
+
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <StatusBar style="auto" />
+      </View>
+</ThemeProvider>
   );
 }
 
