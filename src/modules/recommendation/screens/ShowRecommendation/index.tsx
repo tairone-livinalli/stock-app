f2d1a67fad052cf0570d1c88366dfd17a1b72e92
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-10-12 16:08:54 -0400

    feat: add basic layout and integration with service

--- src/modules/recommendation/screens/ShowRecommendation/index.tsx
+++ src/modules/recommendation/screens/ShowRecommendation/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-import { Container, Title } from './styles'
+import {
+  Container,
+  Recommendation,
+  RecommendationContainer,
+  StockName,
+  Title,
+} from './styles'
+
+import { getRecommendation } from '@services'
 
 export function ShowRecommendation({ route }: any) {
   const insets = useSafeAreaInsets()
+  const { stockSymbol } = route.params
+
+  const [stockRecommendation, setStockRecommendation] = useState<
+    'buy' | 'hold' | 'sell'
+  >('hold')
+
+  useEffect(() => {
+    const callRecommendation = async () => {
+      const recommendation = await getRecommendation(stockSymbol)
+
+      setStockRecommendation(recommendation)
+    }
+
+    callRecommendation()
+  }, [stockSymbol])
 
   return (
     <Container style={{ paddingTop: insets.top }}>
       <Title>Recommendation</Title>
+
+      <RecommendationContainer>
+        <StockName>{stockSymbol}</StockName>
+        <Recommendation recommendation={stockRecommendation}>
+          {stockRecommendation.toUpperCase()}
+        </Recommendation>
+      </RecommendationContainer>
     </Container>
   )
 }
--- src/modules/recommendation/screens/ShowRecommendation/styles.ts
+++ src/modules/recommendation/screens/ShowRecommendation/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native'
 
+type Recommendation = {
+  recommendation: 'buy' | 'sell' | 'hold'
+}
+
 export const Container = styled.View`
   flex: 1;
   justify-content: flex-start;
@@ -12,4 +16,32 @@ export const Title = styled.Text`
   font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   text-align: center;
+`
+
+export const RecommendationContainer = styled.View`
+  margin-top: 32px;
+  padding: 16px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
+`
+
+export const StockName = styled.Text`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`
+
+export const Recommendation = styled.Text<Recommendation>`
+  color: ${({ recommendation, theme }) =>
+    recommendation === 'buy'
+      ? theme.COLORS.BRAND_LIGHT
+      : recommendation === 'sell'
+        ? theme.COLORS.ERROR
+        : theme.COLORS.WHITE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
 `
