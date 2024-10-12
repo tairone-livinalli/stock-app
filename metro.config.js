ebc8b14cfd0101e702177e0567f521747d3b94a5
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-10-12 14:49:01 -0400

    fix: known issue on native date picker for react native > 0.69

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config')
 
-module.exports = getDefaultConfig(__dirname)
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname)
+
+  return {
+    ...config,
+    server: {
+      rewriteRequestUrl: (url) => {
+        if (!url.endsWith('.bundle')) {
+          return url
+        }
+        // https://github.com/facebook/react-native/issues/36794
+        // JavaScriptCore strips query strings, so try to re-add them with a best guess.
+        return (
+          url +
+          '?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true'
+        )
+      },
+    },
+  }
+})()
