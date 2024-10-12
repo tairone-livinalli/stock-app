79f39d3b9592939066808efd7eecea11dbd5ed82
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-10-12 11:09:05 -0400

    chore: add env library

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+    plugins: [
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      [
+        'module:react-native-dotenv',
+        { moduleName: '@env', allowUndefined: false },
+      ],
+    ],
   }
 }
