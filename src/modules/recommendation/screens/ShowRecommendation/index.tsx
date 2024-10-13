0a95fa170ffaad687e0b237d088f49432c588999
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-10-13 18:35:47 -0400

    feat: remove service call and add loading state

--- src/modules/recommendation/screens/ShowRecommendation/index.tsx
+++ src/modules/recommendation/screens/ShowRecommendation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons'
 
@@ -9,10 +9,12 @@ import {
   StockName,
   Title,
   RecommendationContainer,
+  LoadingContainer,
 } from './styles'
 
-import { getRecommendation } from '@services'
 import theme from '@theme'
+import { useRecommendation } from '@hooks'
+import { Loading } from '@components'
 
 const COLOR = {
   buy: theme.COLORS.BRAND_LIGHT,
@@ -35,37 +37,30 @@ export function ShowRecommendation({ route }: any) {
   const insets = useSafeAreaInsets()
   const { stockSymbol } = route.params
 
-  const [stockRecommendation, setStockRecommendation] = useState<
-    'buy' | 'hold' | 'sell'
-  >('hold')
-
-  useEffect(() => {
-    const callRecommendation = async () => {
-      const recommendation = await getRecommendation({ stockSymbol })
-
-      setStockRecommendation(recommendation.action)
-    }
-
-    callRecommendation()
-  }, [stockSymbol])
+  const { action, isLoading } = useRecommendation({
+    stockSymbol,
+    daysAmount: 20,
+  })
 
   return (
     <Container style={{ paddingTop: insets.top }}>
       <Title>Recommendation</Title>
 
-      <RecommendationCard>
-        <StockName>{stockSymbol}</StockName>
-        <RecommendationContainer>
-          <Ionicons
-            name={ICON[stockRecommendation]}
-            size={32}
-            color={COLOR[stockRecommendation]}
-          />
-          <RecommendationText style={{ color: COLOR[stockRecommendation] }}>
-            {stockRecommendation.toUpperCase()}
-          </RecommendationText>
-        </RecommendationContainer>
-      </RecommendationCard>
+      {isLoading ? (
+        <LoadingContainer>
+          <Loading />
+        </LoadingContainer>
+      ) : (
+        <RecommendationCard>
+          <StockName>{stockSymbol}</StockName>
+          <RecommendationContainer>
+            <Ionicons name={ICON[action]} size={32} color={COLOR[action]} />
+            <RecommendationText style={{ color: COLOR[action] }}>
+              {action.toUpperCase()}
+            </RecommendationText>
+          </RecommendationContainer>
+        </RecommendationCard>
+      )}
     </Container>
   )
 }
--- src/modules/recommendation/screens/ShowRecommendation/styles.ts
+++ src/modules/recommendation/screens/ShowRecommendation/styles.ts
@@ -3,8 +3,14 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   flex: 1;
   justify-content: flex-start;
-  padding: 52px;
+  padding: 56px 24px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+`
+
+export const LoadingContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `
 
 export const Title = styled.Text`
