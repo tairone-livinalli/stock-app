3e71174f78ac4d46431088c36ec40168ae75c3b2
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-10-13 18:33:29 -0400

    refactor: change padding and margin size

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  padding: 52px;
+  padding: 56px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_800};
 `
 
@@ -22,11 +22,11 @@ export const Slogan = styled.Text`
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   text-align: center;
 
-  margin-bottom: 52px;
+  margin-bottom: 56px;
 `
 
 export const StockPickerContainer = styled.View`
-  margin-top: 52px;
+  margin-top: 56px;
   justify-content: center;
 `
 export const StockPickerTitle = styled.Text`
